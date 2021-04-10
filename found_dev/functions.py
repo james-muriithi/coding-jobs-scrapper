89d5b55df40bb94d6306bbb455c3d558932794f5
James Muriithi <muriithijames556@gmail.com>
2021-04-10 11:01:54 +0300

    added found.dev

---
+++ found_dev/functions.py
@@ -0,0 +1,106 @@
+# import packages
+import bs4
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+# get soup object
+def get_soup(text):
+	return BeautifulSoup(text, "lxml", from_encoding='utf-8')
+
+
+# extract company
+def extract_company(div): 
+    
+    try:
+        lastDiv = div.find_all('div')[-1]
+        return (lastDiv.find_all('a')[0].text)
+    except:
+        return ''
+
+
+# extract job salary
+def extract_salary(div): 
+    try:
+        lastDiv = div.find_all('div')[-1]
+        salaryIcon = lastDiv.find(name="i", attrs={"class", "fas fa-money-bill color-money"})
+        return (salaryIcon.find_parent('span').text())
+    except:
+        return ''
+    
+
+
+# extract job location
+def extract_location(div):
+    try:
+        lastDiv = div.find_all('div')[-1]
+        locationIcon = lastDiv.find(name="i", attrs={"class", "fas fa-map-marker color-red"})
+        return (locationIcon.find_parent('span').text.strip())
+    except:
+        return ''
+
+
+# extract job title
+def extract_job_title(div):
+    try:
+        company = div.find(name="a", attrs={"class", "h-title"})
+        return (company.text)
+    except:
+        return ''
+
+
+# extract jd summary 
+def extract_summary(url): 
+    try:
+        text = extract_fulltext(url)
+        sentences = text.splitlines()
+        return ' '.join(sentences[0:2])
+    except Exception as e:
+        write_logs(str(e))
+        return ''
+    return ''
+ 
+
+# extract link of job description 
+def extract_link(div): 
+    try:
+        url = "https://found.dev"
+        company = div.find(
+            name="a", attrs={"class", "h-title"})
+        return (url + company.get('href'))
+    except:
+        return ''
+
+
+# extract date of job when it was posted 
+def extract_date(div):
+    try:
+        company = div.find(
+            name="div", attrs={"class", "search-result__job-function"}).find(name="div", attrs={"class", "if-wrapper-column align-self--end text--right"})
+        if company is None:
+            company = div.find(
+                name="div", attrs={"class", "search-result__job-function"}).find(name="div", attrs={"class", "top-jobs__content__time"})
+        return (company.text+ " ago")
+    except:
+        return datetime.now().strftime('%Y-%m-%d')
+
+
+# extract full job description from link
+def extract_fulltext(url):
+    try:
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, "lxml", from_encoding='utf-8')
+        div = soup.find('section', attrs={
+                             'id': 'about-job'})
+        return '\n'.join(div.stripped_strings)
+    except:
+        return ''
+    return ''
+
+
+# write logs to file
+def write_logs(text):
+    # print(text + '\n')
+    f = open('log.txt','a')
+    f.write(text + '\n')  
+    f.close()
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from brightermodays.main import main as brightermodays
 from jobsearchke.main import main as jobsearchke
 from ihub.main import main as ihub
 from linkedin.main import main as linkedin
+from found_dev.main import main as found_dev
 import schedule
 import time
 from dotenv import load_dotenv
@@ -12,8 +13,11 @@ schedule.every(3).hours.do(brightermodays)
 schedule.every(3).hours.do(jobsearchke)
 schedule.every(3).hours.do(ihub)
 schedule.every(3).hours.do(linkedin)
+schedule.every(3).hours.do(found_dev)
 
 if __name__ == "__main__":
+    found_dev()
+    time.sleep(1)
     linkedin()
     time.sleep(1)
     brightermodays()
